97629929a14cecd233c6d26917c00ca798d8b41b
Asgeir <asgeir@cluda.com>
2022-03-15 09:13:27 +0100

    [subgraph] Users can only delete there own articles

--- packages/subgraph/src/article.mapping.ts
+++ packages/subgraph/src/article.mapping.ts
@@ -1,4 +1,4 @@
-import { JSONValue, TypedMap } from "@graphprotocol/graph-ts"
+import { JSONValue, TypedMap, log } from "@graphprotocol/graph-ts"
 import { NewPost } from "../generated/Poster/Poster"
 import { Article } from "../generated/schema"
 import { jsonToString } from "./utils"
@@ -12,7 +12,7 @@ export function handleArticleAction(subAction: String, content: TypedMap<string,
   if (subAction == "create") {
     const articleId = getArticleId(event)
     const article = new Article(articleId)
-    article.poster = event.address
+    article.poster = event.params.user
     article.article = jsonToString(content.get("article"))
     article.title = jsonToString(content.get("title"))
     article.postedOn = event.block.timestamp
@@ -21,6 +21,21 @@ export function handleArticleAction(subAction: String, content: TypedMap<string,
   }
   if (subAction == "delete") {
     const articleId = jsonToString(content.get("id"))
+    const article = Article.load(articleId)
+
+    if (!article) {
+      log.info("Trying to delete unknown article", [articleId])
+      return
+    }
+
+    if (event.params.user != article.poster) {
+      log.warning("The user is unauthorized to delete the article.", [
+        articleId,
+        event.params.user.toHex(),
+        article.poster.toHex(),
+      ])
+      return
+    }
     store.remove(ARTICLE_ENTITY_TYPE, articleId)
   }
 }
--- packages/subgraph/tests/article.test.ts
+++ packages/subgraph/tests/article.test.ts
@@ -65,3 +65,38 @@ test("An account can delete a standalone article that it has created", () => {
   assert.notInStore(ARTICLE_ENTITY_TYPE, articleId)
   clearStore()
 })
+
+test("An account can NOT delete a standalone article that is created by another account", () => {
+  const messageSender = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7")
+  const title = "My First Blog Post"
+  const article = "QmbtLeBCvT1FW1Kr1JdFCPAgsVsgowg3zMJQS8eFrwPP2j"
+  const createArticleContent = `{
+    "action": "article/create",
+    "article": "${article}",
+    "title": "${title}"
+  }`
+
+  const createArticlePostEvent = createNewPostEvent(messageSender, createArticleContent, PUBLICATION_TAG)
+  const articleId = getArticleId(createArticlePostEvent)
+  handleNewPost(createArticlePostEvent)
+
+  // check that the article is in the store
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "title", title)
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "article", article)
+
+  const deleteArticleContent = `{
+    "action": "article/delete",
+    "id": "${articleId}"
+  }`
+
+  const anotherMessageSender = Address.fromString("0xD028d504316FEc029CFa36bdc3A8f053F6E5a6e4")
+
+  const newPostEvent = createNewPostEvent(anotherMessageSender, deleteArticleContent, PUBLICATION_TAG)
+  handleNewPost(newPostEvent)
+
+  // check the article is NOT deleted
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "title", title)
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "article", article)
+
+  clearStore()
+})
