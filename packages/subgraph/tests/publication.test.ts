42c8e71eccbd6b7e96d65645e498f40d31588dd0
Asgeir <asgeir@cluda.com>
2022-03-17 10:56:46 +0100

    [subgraph] Add support for the `article/update` action
    - An account can update a standalone article that it has created
    - An account can update an article in a publication where the account has `article/update` permissions

--- packages/subgraph/tests/publication.test.ts
+++ packages/subgraph/tests/publication.test.ts
@@ -161,3 +161,104 @@ test("An account can NOT an delete an article in a publication (where the accoun
 
   clearStore()
 })
+
+test("An account can update an article in a publication where the account has `article/update` permissions", () => {
+  const user = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7")
+  const publicationTitle = "My First Publication"
+  const publicationContent = `{
+    "action": "publication/create",
+    "title": "${publicationTitle}"
+  }`
+
+  const newPublicationPostEvent = createNewPostEvent(user, publicationContent, PUBLICATION_TAG)
+  const publicationId = getPublicationId(newPublicationPostEvent)
+  handleNewPost(newPublicationPostEvent)
+
+  assert.fieldEquals(PUBLICATION_ENTITY_TYPE, publicationId, "id", publicationId)
+
+  const articleTitle = "My First Blog Post"
+  const article = "QmbtLeBCvT1FW1Kr1JdFCPAgsVsgowg3zMJQS8eFrwPP2j"
+  const articleContent = `{
+    "action": "article/create",
+    "publicationId": "${publicationId}",
+    "article": "${article}",
+    "title": "${articleTitle}"
+  }`
+
+  const newArticlePostEvent = createNewPostEvent(user, articleContent, PUBLICATION_TAG)
+  const articleId = getArticleId(newArticlePostEvent)
+  handleNewPost(newArticlePostEvent)
+
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "title", articleTitle)
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "article", article)
+
+  const updatedTitle = "My First Edited Blog Post"
+  const updatedArticle = "fake_BCvT1FW1Kr1JdFCPAgsVsgowg3zMJQS8eFrwPP2j"
+
+  const updateArticleContent = `{
+    "action": "article/update",
+    "id": "${articleId}",
+    "article": "${updatedArticle}",
+    "title": "${updatedTitle}"
+  }`
+
+  const newUpdatePostEvent = createNewPostEvent(user, updateArticleContent, PUBLICATION_TAG)
+  handleNewPost(newUpdatePostEvent)
+
+  // check that the article is updated
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "title", updatedTitle)
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "article", updatedArticle)
+
+  clearStore()
+})
+
+test("An account can NOT update an article in a publication where the account has does not have any permissions", () => {
+  const user = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7")
+  const publicationTitle = "My First Publication"
+  const publicationContent = `{
+    "action": "publication/create",
+    "title": "${publicationTitle}"
+  }`
+
+  const newPublicationPostEvent = createNewPostEvent(user, publicationContent, PUBLICATION_TAG)
+  const publicationId = getPublicationId(newPublicationPostEvent)
+  handleNewPost(newPublicationPostEvent)
+
+  assert.fieldEquals(PUBLICATION_ENTITY_TYPE, publicationId, "id", publicationId)
+
+  const articleTitle = "My First Blog Post"
+  const article = "QmbtLeBCvT1FW1Kr1JdFCPAgsVsgowg3zMJQS8eFrwPP2j"
+  const articleContent = `{
+    "action": "article/create",
+    "publicationId": "${publicationId}",
+    "article": "${article}",
+    "title": "${articleTitle}"
+  }`
+
+  const newArticlePostEvent = createNewPostEvent(user, articleContent, PUBLICATION_TAG)
+  const articleId = getArticleId(newArticlePostEvent)
+  handleNewPost(newArticlePostEvent)
+
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "title", articleTitle)
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "article", article)
+
+  const updatedTitle = "My First Edited Blog Post"
+  const updatedArticle = "fake_BCvT1FW1Kr1JdFCPAgsVsgowg3zMJQS8eFrwPP2j"
+
+  const updateArticleContent = `{
+    "action": "article/update",
+    "id": "${articleId}",
+    "article": "${updatedArticle}",
+    "title": "${updatedTitle}"
+  }`
+
+  const otherUser = Address.fromString("0xD028d504316FEc029CFa36bdc3A8f053F6E5a6e4")
+  const newUpdatePostEvent = createNewPostEvent(otherUser, updateArticleContent, PUBLICATION_TAG)
+  handleNewPost(newUpdatePostEvent)
+
+  // check that the article is updated
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "title", articleTitle)
+  assert.fieldEquals(ARTICLE_ENTITY_TYPE, articleId, "article", article)
+
+  clearStore()
+})
