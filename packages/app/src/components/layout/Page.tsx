9874d9fd9db0f5b2ec781b2048f376cc7507593d
Julio Avila <javila1997@gmail.com>
2022-03-18 10:18:29 -0300

    Solved merge conflict error

--- packages/app/src/components/layout/Page.tsx
+++ packages/app/src/components/layout/Page.tsx
@@ -1,8 +1,9 @@
 
 import Header from "./Header"
-import { Box } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { useLocation } from "react-router-dom"
 import theme, { palette } from "../../theme"
+import { day } from "../../theme/day"
 
 type Props = {
   title?: string
@@ -12,12 +13,13 @@ type Props = {
 const Page: React.FC<Props> = ({ children, address }) => {
   const location = useLocation();
   return (
-    <>
-      <Header logoColor={location.pathname === "/" ? palette.whites[1000] : theme.palette.text.primary} />
-      <Box component="main" sx={{ pb: 12 }}>
+    <ThemeProvider theme={day}>
+      <CssBaseline />
+      <Header logoColor={location.pathname === "/" ? palette.whites[1000] : theme.palette.text.primary}/>
+      <Box component="main" sx={{pb: 12}}>
         {children}
       </Box>
-    </>
+    </ThemeProvider>
   )
 }
 
