9998ca8f11c2789d1a835652502287513cb7d534
Asgeir <asgeir@cluda.com>
2022-06-28 12:17:56 +0200

    Make it possible for the user to click the desired chain

--- packages/app/src/constants/chain.ts
+++ packages/app/src/constants/chain.ts
@@ -1,3 +1,5 @@
+import { AbstractConnector } from "@web3-react/abstract-connector"
+
 export enum SupportedChainId {
   MAINNET = 1,
   RINKEBY = 4,
@@ -23,4 +25,26 @@ export const chainToString = (chainId: number) => {
       return "Unknown chain"
     }
   }
+}
+
+export const switchChain = async (connector: AbstractConnector, chainId: number) => {
+  const provider = await connector.getProvider()
+  const requiredChainIdHex = `0x${chainId.toString(16)}`
+  return provider!
+    .request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: requiredChainIdHex }],
+    })
+    .catch((error: any) => {
+      if (error.code === 4902) {
+        // the user's wallet does not have this network
+        // return provider!.request({
+        //   method: "wallet_addEthereumChain",
+        //   params: [{ ...desiredChainIdOrChainParameters, chainId: desiredChainIdHex }],
+        // })
+        console.log("User's wallet does not have this network")
+      }
+
+      throw error
+    })
 }
