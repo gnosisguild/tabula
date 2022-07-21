226f0cb9aa8e50d7e23091a659222a2f262a15e9
samepant <sampanter@gmail.com>
2022-07-21 14:25:51 -0400

    adjust tags on article view

--- packages/app/src/components/views/publication/ArticleView.tsx
+++ packages/app/src/components/views/publication/ArticleView.tsx
@@ -110,11 +110,15 @@ export const ArticleView: React.FC<ArticleViewProps> = ({ updateChainId }) => {
                 </Grid>
               )}
               {article.publication && (
-                <Stack alignItems="center" direction="row" spacing={1} my={1}>
+                <Grid container spacing={1} sx={{ marginLeft: -0.5 }}>
                   {article.tags &&
                     article.tags.length > 0 &&
-                    article.tags.map((tag, index) => <Chip label={tag} size="small" key={index} />)}
-                </Stack>
+                    article.tags.map((tag, index) => (
+                      <Grid item>
+                        <Chip sx={{ height: "100%" }} label={tag} size="small" key={index} />
+                      </Grid>
+                    ))}
+                </Grid>
               )}
               <Grid item my={5}>
                 <Markdown>{articleToShow}</Markdown>
