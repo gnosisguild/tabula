721770597f99ab03469d69eaad869fcb5db17829
Colin Spence <udappia@gmail.com>
2022-12-01 09:12:21 -0700

    Adds circle layers back

--- packages/app/src/components/commons/DeterministicAvatar/index.tsx
+++ packages/app/src/components/commons/DeterministicAvatar/index.tsx
@@ -18,7 +18,6 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
   let noiseImg: any
   let layers: any[] = []
   let palette: any[] = []
-  const strokeColor = [43, 2, 20]
   const divisions = 20
 
   let tokenData
@@ -29,18 +28,27 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
   } else {
     tokenData = genTokenData(123)
   }
-  const R = new Random(tokenData)
+  const R: any = new Random(tokenData)
 
   for (let i = 0; i < divisions; i++) {
     palette.push([0, 0, 100]) // white
-    const newFill = [R.random_num(0, 140), R.random_num(30, 65), R.random_num(60, 90)]
+    const newFill = [R.random_num(0, 140), R.random_num(30, 65), R.random_num(70, 90)]
     palette.push(newFill) // color
   }
 
-  function Layer(this: any, size: number, pieces: number, seed: number) {
+  const accentHue = R.random_num(0, 140)
+  const bgColor = [accentHue, R.random_num(10, 30), R.random_num(90, 100)]
+  const strokeColor = [accentHue, R.random_num(20, 40), R.random_num(35, 45)]
+  const outerStrokeColor = [accentHue, R.random_num(50, 80), R.random_num(50, 60)]
+
+  const strokeWeight = 0.012 * width
+
+  function Layer(this: any, size: number, pieces: number, showCircles: boolean, theta: number, fill: any) {
     this.size = size
     this.pieces = pieces
-    this.seed = seed
+    this.showCircles = showCircles
+    this.theta = theta
+    this.fill = fill
   }
 
   function Piece(
@@ -48,7 +56,7 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
     theta: number,
     radius: number,
     rotation: number,
-    showCircles: boolean,
+    // showCircles: boolean,
     fill: any,
     strokeWeight: number,
     strokeColor: any,
@@ -64,7 +72,7 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
     this.outerRadius = radius
     this.rotation = rotation
     this.initialRotation = rotation
-    this.showCircles = showCircles
+    // this.showCircles = showCircles
     this.fill = fill
     this.strokeWeight = strokeWeight
     this.strokeColor = strokeColor
@@ -137,21 +145,6 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
           p5,
         )
       }
-
-      // circle
-      if (this.showCircles) {
-        const cx = this.radius * p5.cos(theta + 45 / 2)
-        const cy = this.radius * p5.sin(theta + 45 / 2)
-        const d = 2 * p5.abs(p5.sin(this.theta / 2)) * this.radius
-        p5.noStroke()
-        // const circleFill = this.fill[3] === 0 ? [this.fill[0], this.fill[1], this.fill[2]] : this.fill
-        p5.fill(this.fill)
-        p5.circle(cx, cy, d * 0.4)
-        p5.noStroke()
-        p5.fill(this.strokeColor)
-        // p5.circle(cx, cy, d * 0.4)
-        drawBrushArc(cx, cy, d * 0.4, this.strokeWeight, 30, p5)
-      }
       p5.pop()
     }
   }
@@ -163,46 +156,44 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
     p5.angleMode(p5.DEGREES)
     p5.imageMode(p5.CENTER)
     p5.ellipseMode(p5.CENTER)
-    markingsLayers = p5.round(R.random_num(4, 10))
+    markingsLayers = p5.round(R.random_num(4, 7))
     p5.strokeJoin(p5.ROUND)
     initializeMarkings(p5)
     initializeNoiseLayer(p5)
   }
 
   const draw = (p5: p5Types) => {
-    const bgColor = [R.random_num(0, 140), R.random_num(10, 30), R.random_num(90, 100)]
     p5.background(bgColor)
     renderMarkings(p5)
     // drawBrushArc(0,0, width*0.4, 5, 300)
     renderNoise(p5)
     p5.noFill()
-    p5.stroke(R.random_choice(palette))
+    p5.stroke(outerStrokeColor)
     p5.strokeWeight(8)
     p5.ellipse(0, 0, width, height)
     p5.noLoop()
   }
 
   const initializeMarkings = (p5: p5Types) => {
-    const layerWithCircles = R.shuffleArray([...Array(markingsLayers)]).slice(0, 3)
+    const layerWithCircles = R.shuffleArray(Array.from(Array(markingsLayers).keys())).slice(0, 2)
     p5.stroke(43, 2, 20)
     for (let i = 0; i < markingsLayers; i++) {
       const pieces = []
       const fillValue = palette[i]
-      const strokeWeight = p5.round(0.004 * width) //p5.round(R.random_num(width * 0.005, height * 0.01))
       const bool_seeds = [R.random_bool(0.5), R.random_bool(0.93)]
       const radius = p5.map(i, 0, markingsLayers, width * 0.43, height * 0.05)
       const isRay = R.random_bool(0.65)
-      const piecesNum = R.random_choice([!isRay ? 4 : null, 8, 16, 24])
+      const piecesNum = R.random_choice([!isRay ? 4 : 0, 8, 16])
+      const rotOffset = isRay && piecesNum === 4 ? 22.5 : 0
       const innerRadius = isRay ? radius * R.random_num(0.5, 0.8) : null
       const theta = 360 / piecesNum
-      // console.log(layerWithCircles)
       for (let p = 0; p < piecesNum; p++) {
         pieces.push(
           new (Piece as any)(
             theta, // theta,
             radius, // radius,
-            theta * p, // rotation,
-            layerWithCircles.some((layer) => layer === i), // showCircle
+            theta * p + rotOffset, // rotation,
+            // layerWithCircles.some((layer: number) => layer === i), // showCircle
             fillValue, // fill
             strokeWeight, // strokeWeight
             strokeColor, // strokeColor
@@ -212,7 +203,15 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
           ),
         )
       }
-      layers.push(new (Layer as any)(p5.map(i, 0, markingsLayers, width * 0.43, height * 0.05), pieces))
+      layers.push(
+        new (Layer as any)(
+          p5.map(i, 0, markingsLayers, width * 0.43, height * 0.05), // size
+          pieces, // pieces
+          layerWithCircles.some((layer: number) => layer === i), // showCircles
+          theta, // theta
+          fillValue, // fill
+        ),
+      )
     }
   }
 
@@ -233,6 +232,23 @@ const DeterministicAvatar: React.FC<DeterministicAvatarProps> = ({ publicationId
       layer.pieces.forEach((piece: any) => {
         piece.render()
       })
+      if (layer.showCircles) {
+        const circleSize = R.random_num(0.2, 0.4)
+        layer.pieces.forEach((piece: any) => {
+          p5.push()
+          p5.translate(width / 2, height / 2)
+          const rotOffset = 22.5
+          p5.rotate(piece.rotation + rotOffset)
+          const cx = layer.size * p5.cos(layer.theta + 45 / 2)
+          const cy = layer.size * p5.sin(layer.theta + 45 / 2)
+          const d = 2 * p5.abs(p5.sin(layer.theta / 2)) * layer.size
+          p5.strokeWeight(strokeWeight)
+          p5.stroke(strokeColor)
+          p5.fill(layer.fill)
+          p5.circle(cx, cy, d * circleSize)
+          p5.pop()
+        })
+      }
     })
     p5.noLoop()
   }
