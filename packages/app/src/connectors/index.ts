0451d4e409e274af9c3eef7d95c4651ec8c21352
Asgeir <asgeir@cluda.com>
2023-01-18 12:40:02 +0100

    Remove some unused code

--- packages/app/src/connectors/index.ts
+++ packages/app/src/connectors/index.ts
@@ -1,8 +1,5 @@
-import { Web3Provider } from "@ethersproject/providers"
 import { InjectedConnector } from "@web3-react/injected-connector"
-import { getLibrary } from "../config"
 import { ALL_SUPPORTED_CHAIN_IDS, SupportedChainId } from "../constants/chain"
-import { NetworkConnector } from "./NetworkConnector"
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
 import { WalletLinkConnector } from "@web3-react/walletlink-connector"
 import TABULA_LOGO_URL from "../assets/images/tabula-logo-wordmark.svg"
@@ -13,6 +10,7 @@ if (typeof INFURA_KEY === "undefined") {
   throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`)
 }
 
+// not used when a wallet is injected (then the wallet's rpc is used)
 const NETWORK_URLS: { [key in SupportedChainId]: string } = {
   [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
@@ -21,16 +19,6 @@ const NETWORK_URLS: { [key in SupportedChainId]: string } = {
   [SupportedChainId.ARBITRUM]: `https://arb1.arbitrum.io/rpc/`,
   [SupportedChainId.OPTIMISM]: `https://mainnet.optimism.io/`,
   [SupportedChainId.OPTIMISM_ON_GNOSIS_CHAIN]: `https://optimism.gnosischain.com`,
-}
-
-export const network = new NetworkConnector({
-  urls: NETWORK_URLS,
-  defaultChainId: SupportedChainId.GOERLI,
-})
-
-let networkLibrary: Web3Provider | undefined
-export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary = networkLibrary ?? getLibrary(network.provider))
 }
 
 export const injected = new InjectedConnector({
