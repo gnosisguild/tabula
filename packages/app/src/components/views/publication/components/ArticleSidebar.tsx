062d18344ee3debe5299cc88dd64e2d38186ad8e
Colin Spence <udappia@gmail.com>
2023-02-13 12:05:22 -0700

    Update ArticleSidebar scrolling/overflows

--- packages/app/src/components/views/publication/components/ArticleSidebar.tsx
+++ packages/app/src/components/views/publication/components/ArticleSidebar.tsx
@@ -28,7 +28,7 @@ const ArticleSidebar: React.FC<ArticleSidebarProps> = ({ showSidebar, setShowSid
   return (
     <Box
       sx={{
-        px: 3,
+        pl: 3,
         transform: `translate(0%)`,
         width: 320,
         position: "relative",
@@ -43,14 +43,16 @@ const ArticleSidebar: React.FC<ArticleSidebarProps> = ({ showSidebar, setShowSid
         },
       }}
     >
-      <Stack spacing={5}>
+      <Stack>
         <Stack
           direction="row"
           sx={{
             alignItems: "center",
+            borderBottom: `1px solid ${palette.grays[200]}`,
             justifyContent: "space-between",
-            mt: 4,
-            height: 40,
+            pb: 3,
+            mt: 5,
+            mr: 3,
           }}
         >
           <Typography variant="h6" fontFamily={typography.fontFamilies.sans} lineHeight="1.5" mt={0}>
@@ -62,56 +64,66 @@ const ArticleSidebar: React.FC<ArticleSidebarProps> = ({ showSidebar, setShowSid
           />
         </Stack>
 
-        {/* Thumbnail */}
-        <Stack spacing={1}>
-          <InputLabel>Thumbnail</InputLabel>
-          <UploadFile defaultImage={article?.image} onFileSelected={setArticleImg} />
-        </Stack>
-
-        {/* Post URL */}
-        <Stack spacing={1}>
-          <InputLabel>Post URL</InputLabel>
-          <Stack spacing={0.5}>
-            <TextField
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LinkIcon />
-                  </InputAdornment>
-                ),
-                sx: {
-                  color: edited ? palette.grays[1000] : palette.grays[600],
-                },
-              }}
-              value="this-is-a-test"
-            />
-            <Typography
-              color={palette.grays[400]}
-              fontSize={12}
-              fontWeight={400}
-              fontFamily={typography.fontFamilies.sans}
-            >
-              gnosis-guild.tabula.gg/this-is-a-test
-            </Typography>
+        <Stack
+          spacing={5}
+          sx={{
+            maxHeight: `calc(100vh - ${theme.spacing(12)})`,
+            overflowY: "scroll",
+            py: 4,
+            pr: 3,
+          }}
+        >
+          {/* Thumbnail */}
+          <Stack spacing={1}>
+            <InputLabel>Thumbnail</InputLabel>
+            <UploadFile defaultImage={article?.image} onFileSelected={setArticleImg} />
           </Stack>
-        </Stack>
 
-        {/* Description */}
-        <Stack spacing={1}>
-          <InputLabel>Description</InputLabel>
-          <TextField multiline minRows={4} />
-        </Stack>
+          {/* Post URL */}
+          <Stack spacing={1}>
+            <InputLabel>Post URL</InputLabel>
+            <Stack spacing={0.5}>
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LinkIcon />
+                    </InputAdornment>
+                  ),
+                  sx: {
+                    color: edited ? palette.grays[1000] : palette.grays[600],
+                  },
+                }}
+                value="this-is-a-test"
+              />
+              <Typography
+                color={palette.grays[400]}
+                fontSize={12}
+                fontWeight={400}
+                fontFamily={typography.fontFamilies.sans}
+              >
+                gnosis-guild.tabula.gg/this-is-a-test
+              </Typography>
+            </Stack>
+          </Stack>
 
-        {/* Tags */}
-        <Stack spacing={1}>
-          <InputLabel>Tags</InputLabel>
-          <CreatableSelect
-            placeholder="Add a tag..."
-            // onSelected={handleTags}
-            // value={tags}
-            value={article?.tags}
-            // errorMsg={tags.length && tags.length >= 6 ? "Add up to 5 tags for your article" : undefined}
-          />
+          {/* Description */}
+          <Stack spacing={1}>
+            <InputLabel>Description</InputLabel>
+            <TextField multiline minRows={4} />
+          </Stack>
+
+          {/* Tags */}
+          <Stack spacing={1}>
+            <InputLabel>Tags</InputLabel>
+            <CreatableSelect
+              placeholder="Add a tag..."
+              // onSelected={handleTags}
+              // value={tags}
+              value={article?.tags}
+              // errorMsg={tags.length && tags.length >= 6 ? "Add up to 5 tags for your article" : undefined}
+            />
+          </Stack>
         </Stack>
       </Stack>
     </Box>
