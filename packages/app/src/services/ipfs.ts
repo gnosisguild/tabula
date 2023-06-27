5ee3b85c513eb240c4ae93b40c26d25c9fa7bc0d
juliopavila <javila1997@gmail.com>
2023-06-27 08:59:01 -0500

    chore: comment browser ipfs node because ipfs-js is deprecated

--- packages/app/src/services/ipfs.ts
+++ packages/app/src/services/ipfs.ts
@@ -41,39 +41,55 @@ export const getClient = async (ipfsNodeEndpoint?: string): Promise<IPFS> => {
       }
     }
 
-    // If none of the HTTP client options are available, we spin up an IPFS node in the browser.
-    try {
-      const ipfsBrowser = await ipfsCore.create({ start: true })
-      if (ipfsBrowser.isOnline()) {
-        console.log("IPFS getClient: using ipfsBrowser")
-        client = ipfsBrowser
-        return client
-      } else {
-        console.log("IPFS setupClient: ipfsBrowser is not online")
-        throw Error("Unable to connect to IPFS node in browser.")
-      }
-    } catch (ipfsBrowserError) {
-      if (ipfsBrowserError instanceof Error && ipfsBrowserError.name === "LockExistsError") {
-        console.log("IPFS setupClient: ipfsBrowser is already running")
-      } else {
-        console.log("Unable to connect to a running IPFS node.")
-        console.log("Will try to connect to a public IPFS node.")
-        for (const endpoint of publicIpfsEndpoints) {
-          try {
-            const ipfsHttpClient = create({ url: endpoint })
-            if (await ipfsHttpClient.version()) {
-              console.log(`IPFS getClient: using ipfsHttpClient at ${endpoint}`)
-              client = ipfsHttpClient
-              return client
-            } else {
-              console.log(`Unable to connect to IPFS node at ${endpoint}`)
-            }
-          } catch (ipfsHttpClientError) {
-            console.log(`Failed to connect to IPFS node at ${endpoint}`)
-          }
+    for (const endpoint of publicIpfsEndpoints) {
+      try {
+        const ipfsHttpClient = create({ url: endpoint })
+        if (await ipfsHttpClient.version()) {
+          console.log(`IPFS getClient: using ipfsHttpClient at ${endpoint}`)
+          client = ipfsHttpClient
+          return client
+        } else {
+          console.log(`Unable to connect to IPFS node at ${endpoint}`)
         }
+      } catch (ipfsHttpClientError) {
+        console.log(`Failed to connect to IPFS node at ${endpoint}`)
       }
     }
+
+    // TODO: ipfs-js is deprecated, the alternative suggested is https://github.com/ipfs/helia
+    // If none of the HTTP client options are available, we spin up an IPFS node in the browser.
+    // try {
+    //   const ipfsBrowser = await ipfsCore.create({ start: true })
+    //   if (ipfsBrowser.isOnline()) {
+    //     console.log("IPFS getClient: using ipfsBrowser")
+    //     client = ipfsBrowser
+    //     return client
+    //   } else {
+    //     console.log("IPFS setupClient: ipfsBrowser is not online")
+    //     throw Error("Unable to connect to IPFS node in browser.")
+    //   }
+    // } catch (ipfsBrowserError) {
+    //   if (ipfsBrowserError instanceof Error && ipfsBrowserError.name === "LockExistsError") {
+    //     console.log("IPFS setupClient: ipfsBrowser is already running")
+    //   } else {
+    //     console.log("Unable to connect to a running IPFS node.")
+    //     console.log("Will try to connect to a public IPFS node.")
+    //     for (const endpoint of publicIpfsEndpoints) {
+    //       try {
+    //         const ipfsHttpClient = create({ url: endpoint })
+    //         if (await ipfsHttpClient.version()) {
+    //           console.log(`IPFS getClient: using ipfsHttpClient at ${endpoint}`)
+    //           client = ipfsHttpClient
+    //           return client
+    //         } else {
+    //           console.log(`Unable to connect to IPFS node at ${endpoint}`)
+    //         }
+    //       } catch (ipfsHttpClientError) {
+    //         console.log(`Failed to connect to IPFS node at ${endpoint}`)
+    //       }
+    //     }
+    //   }
+    // }
   }
 
   return client
