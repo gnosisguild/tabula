c5a2a684beb48a325c3149923257f546b28d320d
Colin Spence <udappia@gmail.com>
2023-09-06 11:49:14 -0600

    Adds zero state for articles

--- packages/app/src/components/views/publication/components/ArticlesSection.tsx
+++ packages/app/src/components/views/publication/components/ArticlesSection.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import { Button, Grid, Typography } from "@mui/material"
+import { Box, Button, Grid, Typography } from "@mui/material"
 import { palette, typography } from "../../../../theme"
 import AddIcon from "@mui/icons-material/Add"
 import { useNavigate, useParams } from "react-router-dom"
@@ -28,53 +28,88 @@ export const ArticlesSection: React.FC = React.memo(() => {
     }
   }, [refetch, data, publicationId])
 
-  return (
-    <>
-      <Grid container justifyContent="space-between" alignItems={"center"} my={4}>
-        <Grid item>
-          <Typography
-            color={palette.grays[1000]}
-            variant="h5"
-            fontFamily={typography.fontFamilies.sans}
-            sx={{ margin: 0 }}
-          >
-            Articles
-          </Typography>
-        </Grid>
-        {havePermissionToCreate && (
+  if (articles && articles.length > 0) {
+    return (
+      <>
+        <Grid container justifyContent="space-between" alignItems={"center"} my={4}>
           <Grid item>
-            <Button
-              variant="contained"
-              size="medium"
-              onClick={() => {
-                navigate(`./new`)
-                setMarkdownArticle(undefined)
-                setArticleEditorState(undefined)
-                saveDraftArticle(INITIAL_ARTICLE_VALUE)
-                saveArticle(undefined)
-                setDraftArticleThumbnail(undefined)
-              }}
+            <Typography
+              color={palette.grays[1000]}
+              variant="h5"
+              fontFamily={typography.fontFamilies.sans}
+              sx={{ margin: 0 }}
             >
-              <AddIcon style={{ marginRight: 13 }} />
-              New Article
-            </Button>
+              Articles
+            </Typography>
           </Grid>
-        )}
-      </Grid>
-      <Grid container flexDirection="column" alignItems="flex-start" justifyContent={"flex-start"} gap={4}>
-        {articles &&
-          articles.length > 0 &&
-          articles.map((article) => (
-            <Grid item sx={{ width: "100%" }} key={article.id || ""}>
-              <ArticleItem
-                article={article}
-                couldUpdate={havePermissionToUpdate}
-                couldDelete={havePermissionToDelete}
-                publicationSlug={publicationSlug || ""}
-              />
+          {havePermissionToCreate && (
+            <Grid item>
+              <Button
+                variant="contained"
+                size="medium"
+                onClick={() => {
+                  navigate(`./new`)
+                  setMarkdownArticle(undefined)
+                  setArticleEditorState(undefined)
+                  saveDraftArticle(INITIAL_ARTICLE_VALUE)
+                  saveArticle(undefined)
+                  setDraftArticleThumbnail(undefined)
+                }}
+              >
+                <AddIcon style={{ marginRight: 13 }} />
+                New Article
+              </Button>
             </Grid>
-          ))}
-      </Grid>
-    </>
+          )}
+        </Grid>
+        <Grid container flexDirection="column" alignItems="flex-start" justifyContent={"flex-start"} gap={4}>
+          {articles &&
+            articles.map((article) => (
+              <Grid item sx={{ width: "100%" }} key={article.id || ""}>
+                <ArticleItem
+                  article={article}
+                  couldUpdate={havePermissionToUpdate}
+                  couldDelete={havePermissionToDelete}
+                  publicationSlug={publicationSlug || ""}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </>
+    )
+  }
+
+  return (
+    <Box
+      sx={{
+        alignItems: "center",
+        borderRadius: 2,
+        bgcolor: palette.grays[100],
+        color: palette.grays[800],
+        display: "flex",
+        fontFamily: typography.fontFamilies.sans,
+        justifyContent: "space-between",
+        my: 4,
+        p: 4,
+        width: "100%",
+      }}
+    >
+      There are currently no articles for this publication. Create one now.
+      <Button
+        variant="contained"
+        size="medium"
+        onClick={() => {
+          navigate(`./new`)
+          setMarkdownArticle(undefined)
+          setArticleEditorState(undefined)
+          saveDraftArticle(INITIAL_ARTICLE_VALUE)
+          saveArticle(undefined)
+          setDraftArticleThumbnail(undefined)
+        }}
+      >
+        <AddIcon style={{ marginRight: 13 }} />
+        New Article
+      </Button>
+    </Box>
   )
 })
